7ec912e6386e1c908027b211799f00d09e3df5ee
Dongminator <p.donglin@gmail.com>
2013-04-04 19:22:42 +0100

    test

--- web.js
+++ web.js
@@ -1,12 +1,37 @@
-var express = require('express');
+var restify = require('restify');
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var server = restify.createServer({
+	name: 'Board Game Clock Server'
 });
 
+
+server.get('/hello/:name', send);
+server.put('/hello', send);
+server.post('/hello', function create(req, res, next) {
+	res.send(201, Math.random().toString(36).substr(3, 8));
+	return next();
+});
+server.del('hello/:name', function rm(req, res, next) {
+	res.send(204);
+	return next();
+});
+
+server.head('/hello/:name', send);
+
+
+
+function send(req, res, next) {
+	res.send('hello ' + req.params.name);
+	return next();
+}
+	
+	
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+server.listen(port, function() {
+	console.log('%s listening at %s', server.name, server.url);
 });
+
+
+
+
+
