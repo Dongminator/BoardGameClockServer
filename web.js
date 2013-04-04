d3adea5f91cc3e8e54f6d1baad76a8aaac992e91
Dongminator <p.donglin@gmail.com>
2013-04-04 21:05:29 +0100

    db

--- web.js
+++ web.js
@@ -1,29 +1,34 @@
 var express = require('express')
-  , app = express.createServer(express.logger())
-  , pg = require('pg').native
-  , connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dailyjs'
-  , start = new Date()
-  , port = process.env.PORT || 3000
-  , client;
+var pg = require('pg');
 
-client = new pg.Client(connectionString);
-client.connect();
+app = express.createServer(express.logger());
+
+
+port = process.env.PORT || 3000;
+
 
 app.get('/', function(req, res) {
-  var date = new Date();
-
-  client.query('INSERT INTO visits(date) VALUES($1)', [date]);
-
-  query = client.query('SELECT COUNT(date) AS count FROM visits WHERE date = $1', [date]);
-  query.on('row', function(result) {
-    console.log(result);
-
-    if (!result) {
-      return res.send('No data found');
-    } else {
-      res.send('Visits today: ' + result.count);
-    }
-  });
+	res.send("lol");
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+		var query = client.query('SELECT * FROM employees');
+		query.on('row', function(row) {
+			console.log(JSON.stringify(row));
+		});
+	});
+//  var date = new Date();
+//
+//  client.query('INSERT INTO visits(date) VALUES($1)', [date]);
+//
+//  query = client.query('SELECT COUNT(date) AS count FROM visits WHERE date = $1', [date]);
+//  query.on('row', function(result) {
+//    console.log(result);
+//
+//    if (!result) {
+//      return res.send('No data found');
+//    } else {
+//      res.send('Visits today: ' + result.count);
+//    }
+//  });
 });
 
 app.listen(port, function() {
